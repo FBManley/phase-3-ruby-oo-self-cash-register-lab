f03181d2e070175074e1552413a3e0647285fdf3
Jake Manley <jbmanley45@gmail.com>
2022-11-26 08:56:49 -0800

    commit tree 07b86e62a629e62934c634303bc29048efad640a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require 'pry'
+class CashRegister
+
+    attr_accessor :discount, :total, :last_transaction, :items
+
+    def initialize(discount=0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+    def add_item(title, amount, quantity=1)
+        self.last_transaction = amount * quantity
+        self.total += self.last_transaction
+        quantity.times do 
+            self.items << title
+        end
+    end
+
+end 
 
